0ad8d4bb425db89a316c9d6b923ab702a972dd89
Vijay Raju <vijaypraju04@gmail.com>
2018-06-19 17:31:50 -0700

    fixed posts reducer"

--- client/src/reducers/postsReducer.js
+++ client/src/reducers/postsReducer.js
@@ -1,22 +1,22 @@
 import { FETCH_POSTS } from '../actions/types';
 
-export default function(state = [], action) {
-  switch (action.type) {
-    case FETCH_POSTS:
-      console.log(action.payload);
-      return action.payload;
-    default:
-      return state;
-  }
-}
-
-// export default function(state = { posts: [], post: {} }, action) {
+// export default function(state = [], action) {
 //   switch (action.type) {
-//     case SHOW_POST:
-//       return { ...state, post: action.payload };
 //     case FETCH_POSTS:
-//       return { ...state, posts: action.payload };
+//       console.log(action.payload);
+//       return action.payload;
 //     default:
 //       return state;
 //   }
 // }
+
+export default function(state = { posts: [], post: {} }, action) {
+  switch (action.type) {
+    case SHOW_POST:
+      return { ...state, post: action.payload };
+    case FETCH_POSTS:
+      return { ...state, posts: action.payload };
+    default:
+      return state;
+  }
+}
